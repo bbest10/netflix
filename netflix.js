16b573ba24f6cb88bb5554b924ddefc601e885df
Brendan Best <87136690+bbest10@users.noreply.github.com>
2022-10-01 00:08:27 -0500

    Update netflix.js

--- netflix.js
+++ netflix.js
@@ -1,43 +1,26 @@
-// const age = -12;
+const answers = document.querySelectorAll('#main')[i];
 
-// if (age>=0 && age < 5  || age ==65) {
-//     console.log("$10");
-    
-// }else if(age>=5 && age<20){
-//     console.log("$20")
-
-// }else if(age>=20 && age<65){
-//     console.log("$50")
-// } 
-// else if(age<0){
-//     console.log("you dont exist yet")
-// }else{
-//     console.log("you are yoo old")
-// }
-
-// const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
-
-// // WRITE YOUR LOOP BELOW THIS LINE:
-// for(let i = 0; i < people.length; i++){
-//     console.log(people[i].toLocaleUpperCase())
-
+// I think this should work. Creating an array of the non selected 
+//  answers, and doing class actions on them when the button is toggled to open
+function buttonClick(index){
+    const answer = answers[index]
+    const otherAnswers = answers.filter((a, i) => i !== index)
+    if (answer.classList.contains('closed')) {
+        answer.classList.remove('closed');
+        answer.classList.add('open');
+        otherAnswers.map(oa => {
+            oa.classList.remove('open')
+            oa.classList.add('closed')
+        })
+        console.log('chop')
+    } else {
+        answer.classList.remove('open');
+        answer.classList.add('closed');
+    }
+}
 
 
 for (let i = 0; i < document.querySelectorAll('.faq-div').length; i++) {
     // const element = array[i];
-    document.querySelectorAll('.faq-div')[i].addEventListener('click', buttonClick)
-    let answer = document.querySelectorAll('#main')[i];
-
-
-function buttonClick(){
-
-if (answer.classList == 'closed') {
-    answer.classList.add('open');
-    answer.classList.remove('closed');
-    console.log('chop')
-}else{
-    answer.classList.add('closed');
-    answer.classList.remove('open');
-}
-}
+    document.querySelectorAll('.faq-div')[i].addEventListener('click', () => buttonClick(i))
 }
